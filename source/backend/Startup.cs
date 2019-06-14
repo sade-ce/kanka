4239c8780628247223148a058f128f210c5d10e4
zinderud <mokosam@gmail.com>
2019-06-15 01:19:00 +0300

    #2 docker compose finish

--- source/backend/Startup.cs
+++ source/backend/Startup.cs
@@ -25,7 +25,7 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
