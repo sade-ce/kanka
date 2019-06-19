8b96efc7fa175049c80958cee32c04bab11143bd
zinderud <mokosam@gmail.com>
2019-06-19 13:30:40 +0300

    #2  docker finish

--- source/backend/entrypoint.sh
+++ source/backend/entrypoint.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 
-until dotnet ef -s backend -p backend database update; do
+set -e
+run_cmd="dotnet watch  run"
+
+dotnet restore
+
+until dotnet ef database update; do
 >&2 echo "DB is starting up"
 sleep 1
 done
