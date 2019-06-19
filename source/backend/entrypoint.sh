f6bc5debe5b24704a64afcb19c1ee9b814add6a3
zinderud <mokosam@gmail.com>
2019-06-19 11:43:31 +0300

    docker

--- source/backend/entrypoint.sh
+++ source/backend/entrypoint.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
 
-set -e
-run_cmd="dotnet run --server.urls http://*:5000"
-until dotnet ef database update; do
->&2 echo "SQL Server is starting up"
+until dotnet ef -s backend -p backend database update; do
+>&2 echo "DB is starting up"
 sleep 1
 done
 
->&2 echo "SQL Server is up - executing command"
+>&2 echo "DB is up - executing command"
 exec $run_cmd
-
