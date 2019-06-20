0bfb6b4dcfd17d87cd5d2a39d7c68ee4af8a4ea0
zinderud <mokosam@gmail.com>
2019-06-20 11:04:38 +0300

    junk files deleted

--- source/backend/Enums/Vote.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.Enums
-{
-    public enum Vote
-    {
-        Dislike = -1,
-        None = 0,
-        Like = 1
-    }
-}
--- source/backend/ViewModels/CurrencyFilter.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Server.ViewModels
-{
-    public class CurrencyFilter
-    {
-        public string searchString { get; set; }
-    }
-}
