b5332ed2400fc71d954353ca45cda66699d342dc
zinderud <mokosam@gmail.com>
2019-06-24 13:49:38 +0300

    #12 layout role  düzenlemesi

--- source/frontend/src/app/shared/components/layout/layout.component.ts
+++ source/frontend/src/app/shared/components/layout/layout.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { Menu } from '@app/core/models/menu.model';
+import { environment as env} from '@env/environment';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AuthService } from '@app/core/services/core';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
+  isLoggedIn$: Observable<boolean>;
+  username$: Observable<string>;
+  public isAdmin$ = new BehaviorSubject(false);
+  public isManager$ = new BehaviorSubject(false);
 
-  constructor() { }
+  subscription: Subscription;
+
+
+
+// todo: yetki sevivesine göre  menüyü görüntüleme
+  dashboard: Menu;
+  adminMenu: Menu;
+  managerMenu: Menu;
+
+
+  envName = env.envName;
+  appName = env.appName;
+  year = new Date().getFullYear();
+  isProd = env.production;
+
+
+  // kullanım https://alligator.io/angular/breakpoints-angular-cdk/
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches)
+  );
+  constructor(private breakpointObserver: BreakpointObserver,
+              private authService: AuthService,
+              private snackBar: MatSnackBar,
+              private router: Router) { }
 
   ngOnInit() {
+    this.isLoggedIn$ = this.authService.isAuthenticated();
+    this.username$ = this.authService.getUsernameAsync();
+
+    this.subscription = this.authService.getRoleAsync().subscribe( (data: any) => {
+
+        if (data && typeof data === 'object' && data.constructor === Array)  {
+          if (data.some(x => x === 'Admin')) {
+            this.isAdmin$.next(true);
+          }
+          if (data.some(x => x === 'Manager')) {
+            this.isManager$.next(true);
+          }
+        } else {
+          if (data === 'Admin') {
+            this.isAdmin$.next(true);
+          }
+          if (data === 'Manager') {
+            this.isManager$.next(true);
+          }
+        }
+    });
   }
 
+
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
