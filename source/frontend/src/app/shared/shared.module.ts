3f10a62d955bf3dfff3dbef715a9d62ae1dc8d1a
zinderud <mokosam@gmail.com>
2019-06-24 10:43:22 +0300

    #5 confirm component add

--- source/frontend/src/app/shared/shared.module.ts
+++ source/frontend/src/app/shared/shared.module.ts
@@ -4,9 +4,14 @@ import { ConfirmComponent } from './components/confirm/confirm.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { GuestSelectorComponent } from './components/guest-selector/guest-selector.component';
+import { CustomMatPaginatorIntl } from './lib/mat-paginator-intl';
+import { MatPaginatorIntl } from '@angular/material';
 
 @NgModule({
   declarations: [ConfirmComponent, LayoutComponent, NavMenuComponent, GuestSelectorComponent],
+  providers: [
+    {provide: MatPaginatorIntl, useClass: CustomMatPaginatorIntl}
+  ],
   imports: [
     CommonModule
   ]
