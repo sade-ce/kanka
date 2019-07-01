5ceaecbb9ff2f2612f7c511259df3afab6375a3e
zinderud <mokosam@gmail.com>
2019-07-01 10:56:44 +0300

    #13 and #14  Patient

---
+++ lib/kps/kpsweb/model/patient.cs
@@ -0,0 +1,23 @@
+using System;
+namespace kpsweb{
+
+
+
+    public class Patient:BaseModel{
+
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MotherName { get; set; }
+        public long MotherTc { get; set; }
+        public string FatherName { get; set; }
+        public long FatherTc { get; set; }
+        public bool sex {get;set;}
+        public DateTime birthday { get; set; }
+        public string BirthPlace { get; set; }
+        public bool Married { get; set; }
+        public string status { get; set; }
+
+
+    }
+}
